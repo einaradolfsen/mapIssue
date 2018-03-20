302d8636e5b084757ecd2a4ef15366b4d19be293
einar adolfsen <einaradolfsen@lyse.net>
2018-03-20 20:55:02 +0100

    readme

--- app.js
+++ app.js
@@ -1,3 +1,28 @@
-export class App {
-  message = 'hello world';
+export class App { 
+  message = "Languages"; 
+  constructor() {
+    this.selectedLanguages = [];
+    this.map = this.seadMap();
+  }
+
+  seadMap(){
+    let languages = new Map();
+    languages.set("A", [
+      "Afrikaans",
+      "Albanian",
+      "Amharic"
+    ]);
+    languages.set("B", [
+      "Bamanankan",
+      "Bashkort (Bashkir)",
+      "Basque"
+    ]);
+    languages.set("C", [
+      "Cantonese",
+      "Catalan",
+      "Cebuano"
+    ]);
+    
+    return languages;
+  }
 }
